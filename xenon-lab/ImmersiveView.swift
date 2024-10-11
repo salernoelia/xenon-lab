695a06ffe5509be83118bd042820c4fc6370cba6
salernoelia <putopet@gmail.com>
2024-10-11 12:47:13 +0200

    collision detection

--- xenon-lab/ImmersiveView.swift
+++ xenon-lab/ImmersiveView.swift
@@ -8,32 +8,62 @@
 import SwiftUI
 import RealityKit
 import RealityKitContent
+import Combine
 import UmainSpatialGestures
 
 struct ImmersiveView: View {
     @Environment(AppModel.self) var appModel
-   
     @State var tileEntity = Entity()
-
+    @State var collision1Entity = Entity()
+    @State var collision2Entity = Entity()
+    @State private var subscription: EventSubscription?
+    
     var body: some View {
         RealityView { content in
-            // Add the initial RealityKit content
             if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
                 content.add(immersiveContentEntity)
                 
-                if let tileEntityQuery = immersiveContentEntity.findEntity(
-                    named: "sample_tile_4"
-                ) {
-                    tileEntity = tileEntityQuery
-                                 
+                if let collision1EntityQuery = immersiveContentEntity.findEntity(named: "plate1") {
+                    collision1Entity = collision1EntityQuery
+                    print(collision1Entity)
+                    
+                    subscription = content.subscribe(to: CollisionEvents.Began.self, on: collision1Entity) { event in
+                        print("Collision started between \(event.entityA.name) and \(event.entityB.name)")
+                    }
+                    subscription = content
+                        .subscribe(
+                            to: CollisionEvents.Ended.self,
+                            on: collision1Entity
+                        ) { event in
+                        print("Collision ended between \(event.entityA.name) and \(event.entityB.name)")
+                    }
+                  
                 }
                 
+                if let collision2EntityQuery = immersiveContentEntity.findEntity(named: "plate2") {
+                    collision2Entity = collision2EntityQuery
+                    print(collision2Entity)
+                    
+                    subscription = content.subscribe(to: CollisionEvents.Began.self, on: collision2Entity) { event in
+                        print("Collision started between \(event.entityA.name) and \(event.entityB.name)")
+                        
+                    }
+                    subscription = content.subscribe(to: CollisionEvents.Ended.self, on: collision2Entity) { event in
+                        print("Collision started between \(event.entityA.name) and \(event.entityB.name)")
+                        
+                    }
+                }
+
+                if let tileEntityQuery = immersiveContentEntity.findEntity(named: "sample_tile_4") {
+                    tileEntity = tileEntityQuery
+                }
             }
         }
-        .onChange(of: tileEntity.position) { _, newValue in
-            print(tileEntity.position)
-            print(newValue)
-        }
         .useDragGesture()
+    }
+    
+    
+    func logEntity(_ entity: Entity) {
+        print("\(entity)")
     }
 }
